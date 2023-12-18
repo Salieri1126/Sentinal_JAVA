aa26ebae188a2632a8a86cde7beaa592811f83c3
jyhan <wndus5357@naver.com>
2023-12-18 16:10:35 +0900

    2023-12-18 16:10 / jyhan

--- src/main/java/com/example/demo/controller/HomeLogController.java
+++ src/main/java/com/example/demo/controller/HomeLogController.java
@@ -5,6 +5,7 @@ import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -38,6 +39,25 @@ public class HomeLogController {
 
 	    List<ReadLogsEntity> logs = new ArrayList<>();
 	    logs.addAll(readLogsMapper.getTodayLogs(tableName, today.atStartOfDay(), today.atTime(23, 59, 59)));
+	    return logs;
+	}
+	
+	@GetMapping("/admin/menu/home/api/logWeek")
+	@ResponseBody
+	public List<ReadLogsEntity> getWeekLogs() {
+		LocalDate today = LocalDate.now();
+	    LocalDate oneWeekAgo = today.minusDays(7);
+
+	    List<ReadLogsEntity> logs = new ArrayList<>();
+	    for (LocalDate date = oneWeekAgo; !date.isAfter(today); date = date.plusDays(1)) {
+	        String tableName = "log_" + date.format(DateTimeFormatter.ofPattern("yyyyMMdd"));
+	        try {
+	        	logs.addAll(readLogsMapper.getWeekLogs(tableName, date.atStartOfDay(), date.atTime(23, 59, 59)));
+	    
+	        } catch (DataAccessException e) {
+	        	e.printStackTrace();
+	        }
+	    }
 	    return logs;
 	}
 }
