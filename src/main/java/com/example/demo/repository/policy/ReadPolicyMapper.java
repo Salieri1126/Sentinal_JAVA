99735fc40dda96b4b135c92833cb5f294fc5f828
Rud2K <rudgh2647k@naver.com>
2023-12-18 16:11:26 +0900

    2023-12-18 16:11 / khchoi

--- src/main/java/com/example/demo/repository/policy/ReadPolicyMapper.java
+++ src/main/java/com/example/demo/repository/policy/ReadPolicyMapper.java
@@ -3,6 +3,7 @@ package com.example.demo.repository.policy;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import com.example.demo.model.policy.ReadPolicyEntity;
 
@@ -10,5 +11,5 @@ import com.example.demo.model.policy.ReadPolicyEntity;
 public interface ReadPolicyMapper {
     List<ReadPolicyEntity> findAll();
     ReadPolicyEntity findByDetectedName(String detected_name);
-    void updatePolicyEnable(EnableRequestEntity policyUpdateRequest);
+    void updatePolicyEnable(@Param("detected_no") Integer detected_no, @Param("enable") Integer enable);
 }
