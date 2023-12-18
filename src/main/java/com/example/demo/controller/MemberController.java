71bed3c515b391630104e3879581ee3a5063a7d1
Rud2K <rudgh2647k@naver.com>
2023-12-18 20:21:48 +0900

    2023-12-18 20:21 / khchoi

--- src/main/java/com/example/demo/controller/MemberController.java
+++ src/main/java/com/example/demo/controller/MemberController.java
@@ -1,10 +1,13 @@
 package com.example.demo.controller;
 
+import java.security.NoSuchAlgorithmException;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.demo.model.user.MemberEntity;
 import com.example.demo.service.MemberService;
@@ -34,7 +37,7 @@ public class MemberController {
     }
 
     @PostMapping("/admin")
-    public String login(@ModelAttribute MemberEntity memberEntity, Model model, HttpSession session) {
+    public String login(@ModelAttribute MemberEntity memberEntity, Model model, HttpSession session) throws NoSuchAlgorithmException {
         if (memberService.login(memberEntity)) {
         	session.setAttribute("id", memberEntity);
         	session.setMaxInactiveInterval(30 * 60);
@@ -43,6 +46,16 @@ public class MemberController {
             return "redirect:/admin";
         }
     }
+    
+	@PostMapping("/admin/menu/changePassword")
+	public String changePassword(@RequestParam String currentPassword, @RequestParam String newPassword) throws NoSuchAlgorithmException {
+		boolean isChanged = memberService.changePassword(currentPassword, newPassword);
+		if (isChanged) {
+			return "redirect:/admin/menu/setup?changePassword=true";
+		} else {
+			return "redirect:/admin/menu/setup?changePassword=false";
+		}
+	}
     
     @GetMapping("/logout")
     public String logout(HttpSession session) {
--- src/main/java/com/example/demo/model/user/MemberEntity.java
+++ src/main/java/com/example/demo/model/user/MemberEntity.java
@@ -12,4 +12,5 @@ public class MemberEntity {
     private Integer personal_no;
     private String id;
     private String password;
+    private String salt;
 }
